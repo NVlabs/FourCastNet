fcb8949c0bf15d8d9e5270fedbc1a5542746e810
Filip Øskar Łanecki <9296944+orhid@users.noreply.github.com>
2023-07-21 05:01:13 -0300

    mój błąd, sorka

--- datafactory/split.py
+++ datafactory/split.py
@@ -16,13 +16,15 @@ args = parser.parse_args()
 data = h5py.File(args.data_path_input, 'r')[HDF_DATASET]
 
 # -- select data
-# -- save standardized data
-with h5py.File(args.data_path_output, 'a') as f:
-    if HDF_DATASET in f.keys():
-        del f[HDF_DATASET]
-    # are we not saving the data twice here ??
-    f.create_dataset(HDF_DATASET,
-                     data=data,
-                     shape=data.shape,
-                     dtype=np.float32)
-    f[HDF_DATASET][...] = data
+chunk = int(data.shape[0] / args.splits)
+for split in range(args.splits):
+    file_name = f'{args.data_path_input[:-3]}_split_{split}.h5'
+    with h5py.File(file_name, 'a') as f:
+        if HDF_DATASET in f.keys():
+            del f[HDF_DATASET]
+        # are we not saving the data twice here ??
+        f.create_dataset(HDF_DATASET,
+                         data=data[split * chunk:(split + 1) * chunk],
+                         shape=data.shape,
+                         dtype=np.float32)
+        # f[HDF_DATASET][...] = data
